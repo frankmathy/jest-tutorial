c196687caaeb8283b19ba665df6bff434511ffab
Frank Mathy <frankmathy@gmail.com>
2021-02-07 13:29:22 +0100

    Finished tutorial

--- functions.js
+++ functions.js
@@ -1,5 +1,30 @@
 const axios = require('axios');
 
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+// beforeAll(() => initDatabase());
+// afterAll(() => closeDatabase());
+
+// const initDatabase = () => console.log('Database initialized...');
+// const closeDatabase = () => console.log('Database closed...');
+
+//const nameCheck = () => console.log('Checking Name...');
+
+describe('Checking Names', () => {
+  //beforeEach(() => nameCheck());
+
+  test('User is Jeff', () => {
+    const user = 'Jeff';
+    expect(user).toBe('Jeff');
+  });
+
+  test('User is Karen', () => {
+    const user = 'Karen';
+    expect(user).toBe('Karen');
+  });
+});
+
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
